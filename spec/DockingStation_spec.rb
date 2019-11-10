be205eb76b3f6c9ec5954a8f5932749990f8e5a7
Simone <simonewlkr@hotmail.com>
2019-11-10 12:51:28 +0000

    Eight commit - failed then passed the unit test added method bike

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -31,4 +31,13 @@ describe DockingStation do
     bike = docking_station.release_bike#Creates an instance of the bike
     expect(docking_station).to respond_to(:dock).with(1).argument #we want the method dock to pass the bike instance as an argument dock(bike)
   end
+
+  #Checks a bike method is responsive to the docking_station but not sure why it's created
+  it { is_expected.to respond_to(:bike) }
+
+  # it 'should pass bike instance into the dock method' do
+  #   docking_station = DockingStation.new #Creates an instance of the docking station
+  #   bike = docking_station.release_bike#Creates an instance of the bike
+  # expect(docking_station.dock).to eq bike
+  # end
 end
